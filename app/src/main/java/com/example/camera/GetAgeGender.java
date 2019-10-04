52afb29ecb565d6efd08836a11b0724297e82d09
Marina <marinataran27@gmail.com>
2019-10-04 07:18:31 +0300

    pull request

--- app/src/main/java/com/example/camera/GetAgeGender.java
+++ app/src/main/java/com/example/camera/GetAgeGender.java
@@ -9,6 +9,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.gson.JsonArray;
@@ -32,21 +34,34 @@ import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 
-public class GetAgeGender extends AppCompatActivity {
+import static android.widget.Toast.LENGTH_LONG;
 
+public class GetAgeGender extends AppCompatActivity {
+Button button;
     private final OkHttpClient mOkHttpClient = new OkHttpClient();
     private String mAccessToken ="b9d1ed1f20f04446b190f7a54ffa578c";
     private Call mCall;
     public static final MediaType JSON
             = MediaType.parse("application/json; charset=utf-8");
     private Call mcal;
-
+TextView age;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_age_gender);
+        age=findViewById(R.id.editText);
+button=findViewById(R.id.gender);
+button.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(GetAgeGender.this, MainActivity.class);
+        age=(TextView) intent.getSerializableExtra("qwer");
 
+//        Intent intent=this.getIntent();
+//        age= (TextView) intent.getSerializableExtra("qwer");
+    }
+});
 
     }
 
